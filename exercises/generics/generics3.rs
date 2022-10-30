6abc63689cd6d2b0838bfd2b47a61d7ed3eb01d0
Zhongya.Yan <yanzhongya@shizhuang-inc.com>
2022-10-30 23:03:59 +0800

    做联系

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -10,15 +10,13 @@
 
 // Execute 'rustlings hint generics3' for hints!
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
-    pub student_name: String,
+pub struct ReportCard<T,F> {
+    pub grade: F,
+    pub student_name: T,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl <T: std::fmt::Display,F: std::fmt::Display> ReportCard<T,F> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -46,7 +44,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
